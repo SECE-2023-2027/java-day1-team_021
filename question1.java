59582b9e1c25551296124c2769799e89f453de14
vishnu-r-2023 <vishnu.r2023csbs@sece.ac.in>
2026-06-15 16:04:46 +0530

    Vishnu R- Qn1

---
+++ question1.java
@@ -0,0 +1,13 @@
+package Vishnu;
+
+import java.util.*;
+
+public class question1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("Hello");
+        System.out.println(name);
+        sc.close();
+    }
+}
